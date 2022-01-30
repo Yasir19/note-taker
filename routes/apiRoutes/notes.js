697ba3e2090965359f53de3a047435d2714d637f
yasir19 <yasir.habboo@gmail.com>
2022-01-30 00:19:12 -0600

    add Heroku

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -61,7 +61,9 @@ res.json(response);
 // endpoint 
 router.delete('/notes/:id',(req,res)=>{
      console.log("req params", req.params.id)
-     const deletedItem = notes.filter(({ id }) => id != req.params.id);
+     const deletedItem = notes.filter( id =>{
+         return id.id != req.params.id;
+     })
      fs.writeFile('./db/db.json', JSON.stringify(deletedItem, null, 2),
      (writeERR) =>
      writeERR
